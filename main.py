80a97992f6c6be0c7d87449759389a928fe8a754
Umang-Kumar <76547661+Umang-Kumar@users.noreply.github.com>
2021-09-22 17:10:05 +0530

    login and signup connected to database

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, request
+import mysql.connector
+from flask_mail import Mail
+import json
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import pymysql
 
 
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/tpo_site'
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['mail_user'],
+    MAIL_PASSWORD=params['mail-password']
+)
+mail = Mail(app)
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+
 db = SQLAlchemy(app)
-pymysql.install_as_MySQLdb()
 
 
 class Contacts(db.Model):
@@ -19,23 +34,70 @@ class Contacts(db.Model):
     date = db.Column(db.String(12), nullable=True)
 
 
+class Signup(db.Model):
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False, primary_key=True)
+    password = db.Column(db.String(25), nullable=False)
+
+
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 
 @app.route("/inner-pages")
 def inside():
-    return render_template('inner-page.html')
+    return render_template('inner-page.html', params=params)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    log_db = mysql.connector.connect(
+        host=params['host'],
+        user=params['user'],
+        password=params['password'],
+        database=params['database']
+    )
+    log_cursor = log_db.cursor()
+    if request.method == 'POST':
+        log = request.form
+        email = log['email']
+        password = log['password']
+        log_cursor.execute("select * from signup where email = '" + email + "' and password = '" + password + "'")
+        log_cursor.fetchall()
+        count = log_cursor.rowcount
+        if count == 1:
+            log_db.commit()
+            log_cursor.close()
+            return render_template('inner-page.html', params=params)
+        else:
+            return "Wrong credentials."
     return render_template('Login.html')
 
 
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    sign_db = mysql.connector.connect(
+        host=params['host'],
+        user=params['user'],
+        password=params['password'],
+        database=params['database']
+    )
+    sign_cursor = sign_db.cursor()
+    if request.method == 'POST':
+        sign = request.form
+        name = sign['name']
+        email = sign['email']
+        password = sign['password']
+        re_password = sign['con_pass']
+        if password == re_password:
+            sign_cursor.execute("insert into signup (name, email, password, con_pass) values(%s, %s, %s, %s)",
+                                (name, email, password, re_password))
+            sign_db.commit()
+            sign_cursor.close()
+            return "Registered Successfully"
+        else:
+            return "Your password don't match."
     return render_template('signup page.html')
 
 
@@ -50,7 +112,11 @@ def contact():
         entry = Contacts(name=name, email=email, subject=subject, message=message, date=datetime.now())
         db.session.add(entry)
         db.session.commit()
-    return render_template('contact.html')
+        mail.send_message("New message from " + name,
+                          sender=email,
+                          recipients=[params['mail_user']],
+                          body=message)
+    return render_template('contact.html', params=params)
 
 
 app.run(debug=True)
